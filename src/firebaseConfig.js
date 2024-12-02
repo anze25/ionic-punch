a90237cfe8a2ad0fa4f6349b331a234e21367a99
anze25 <anze20@hotmail.com>
2024-12-02 17:38:24 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDLk_EFvaH3sY69dH6BwUo4ypdHk_OeCJI',
-  authDomain: 'qpunch-88a40.firebaseapp.com',
-  databaseURL: 'https://qpunch-88a40-default-rtdb.firebaseio.com',
-  projectId: 'qpunch-88a40',
-  storageBucket: 'qpunch-88a40.firebasestorage.app',
-  messagingSenderId: '955337406238',
-  appId: '1:955337406238:web:b4c3ad754268326ae2fb0f',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 const app = initializeApp(firebaseConfig);
